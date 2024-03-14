fc6dbca12d13ca5fa9046236b8dbcc482862d628
HwangMing2105 <157661893+HwangMing2105@users.noreply.github.com>
2024-03-14 08:02:04 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,22 @@
 public class Book {
+  private String author;
+    private int numberOfPages;
+    private String genre;
+    public Book (String name, String description, String ID, float price, String author, int num, String genre){
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = num;
+        this.genre = gen;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("PRICE " + price);
+        System.out.println("Author: " + author);
+        System.out.println("numberOfPages: " + numberOfPages);
+        System.out.println("Genre: " + genre);
+    }
 }
