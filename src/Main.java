4328939ccba6657e45d613d312f3edbf1c44c0dc
HwangMing2105 <157661893+HwangMing2105@users.noreply.github.com>
2024-03-14 08:06:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Tạo đối tượng MP3
+        MP3 mp3 = new MP3("MP3 Player", "Portable", "MP3001", 49.99f, 240);
 
-        MP3 mp3;
+        // Tạo đối tượng Book
+        Book book = new Book("Java Programming", "Learn Java programming language", "BK1001", 29.99f, "John Doe", 299, "Academic");
+
+        // Hiển thị thông tin của đối tượng MP3
+        System.out.println("Information of MP3:");
         mp3.showInfo();
+        System.out.println();
 
-        Book book;
+        // Hiển thị thông tin của đối tượng Book
+        System.out.println("Information of Book:");
         book.showInfo();
-
     }
+}
+
 }
